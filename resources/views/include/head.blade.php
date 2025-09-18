deb670e88deb5bf64e7c1e0159fab77522f21b1f
Frank <ing.frankcatamo@gmail.com>
2025-09-18 00:37:25 -0400

    Fix and optimization

--- resources/views/include/head.blade.php
+++ resources/views/include/head.blade.php
@@ -4,13 +4,12 @@
       <head>
         <meta charset="utf-8">
         <!--Import Google Icon Font-->
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" defer>
         <!--Import materialize.css-->
-        <link type="text/css" rel="stylesheet" href="{{ asset('css/materialize.min.css') }}"  media="screen,projection">
-        <link type="text/css" rel="stylesheet" href="{{ asset('css/master.min.css') }}">
+        <link type="text/css" rel="stylesheet" href="{{ asset('css/materialize.min.css') }}"  media="screen,projection" async>
+        <link type="text/css" rel="stylesheet" href="{{ asset('css/master.min.css') }}" async>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <title>Ing. Frank Catamo</title>
-        <!-- <meta name="description" content="Portafolio personal y profesional de Frank Catamo."> -->
         <meta name="description" content="Portafolio personal y profesional de Frank Catamo.">
         <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}">
         <script src="https://www.google.com/recaptcha/api.js?render=6Ldm8a4qAAAAAE_Cw_bnj3F7vMub8EjGoDgeE7g7"></script>
