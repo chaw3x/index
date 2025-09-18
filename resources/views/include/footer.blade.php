0e38fe7238ddfc428cb24d022f2ca0aa8f19d0a0
Frank <ing.frankcatamo@gmail.com>
2025-09-18 01:04:01 -0400

    Fix and optimization

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -26,7 +26,7 @@
 <!--JavaScript at end of body for optimized loading-->
 <!--Let browser know website is optimized for mobile-->
 <script src="{{ asset('js/jquery-3.6.0.min.js') }}" charset="utf-8"></script>
-<script type="text/javascript" src="{{ asset('js/materialize.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/materialize.min.js')}}?v={{ env('VERSION_PUBLIC') }}"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script type="text/javascript">
     $(document).ready(function(){
--- resources/views/include/head.blade.php
+++ resources/views/include/head.blade.php
@@ -7,11 +7,11 @@
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" defer>
         <!--Import materialize.css-->
         <link type="text/css" rel="stylesheet" href="{{ asset('css/materialize.min.css') }}"  media="screen,projection" async>
-        <link type="text/css" rel="stylesheet" href="{{ asset('css/master.min.css') }}" async>
+        <link type="text/css" rel="stylesheet" href="{{ asset('css/master.min.css') }}?v={{ env('VERSION_PUBLIC') }}" async>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <title>Ing. Frank Catamo</title>
         <meta name="description" content="Portafolio personal y profesional de Frank Catamo.">
-        <link rel="icon" href="{{ asset('images/icon.ico') }}">
-        <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}">
+        <link rel="icon" href="{{ asset('images/icon.ico') }}?v={{ env('VERSION_PUBLIC') }}">
+        <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}?v={{ env('VERSION_PUBLIC') }}">
         <script src="https://www.google.com/recaptcha/api.js?render=6Ldm8a4qAAAAAE_Cw_bnj3F7vMub8EjGoDgeE7g7"></script>
     </head>
