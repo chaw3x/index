950db30006013f659355f6cb4fdf5805db90db5a
Frank <ing.frankcatamo@gmail.com>
2025-09-18 23:00:56 -0400

    Fix and optimization

--- resources/views/include/head.blade.php
+++ resources/views/include/head.blade.php
@@ -5,13 +5,13 @@
         <!--Import Google Icon Font-->
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" defer>
         <!--Import materialize.css-->
-        <link type="text/css" rel="stylesheet" href="{{ asset('css/materialize.min.css') }}"  media="screen,projection" async>
-        <link type="text/css" rel="stylesheet" href="{{ asset('css/master.min.css') }}?v={{ env('VERSION_PUBLIC') }}" async>
+        <link type="text/css" rel="preload" href="{{ asset('css/materialize.min.css') }}"  media="screen,projection" as="style">
+        <link type="text/css" rel="stylesheet" href="{{ asset('css/master.min.css') }}?v={{ env('VERSION_PUBLIC') }}" defer>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <title>Ing. Frank Catamo</title>
         <meta name="description" content="Portafolio personal y profesional de Frank Catamo.">
         <link rel="shortcut icon" href="{{ asset('images/icon.ico') }}?v={{ env('VERSION_PUBLIC') }}" type="image/x-icon">
         <link rel="icon" href="{{ asset('images/logo.png') }}?v={{ env('VERSION_PUBLIC') }}" type="image/png">
-        <script src="https://www.google.com/recaptcha/api.js?render=6Ldm8a4qAAAAAE_Cw_bnj3F7vMub8EjGoDgeE7g7"></script>
+        <script src="https://www.google.com/recaptcha/api.js?render=6Ldm8a4qAAAAAE_Cw_bnj3F7vMub8EjGoDgeE7g7" defer></script>
     </head>
     <body>
